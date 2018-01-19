0f590c92531a42c39246b66cdda19910ca9943b3
Narek <35571991+narekC@users.noreply.github.com>
2018-01-19 15:48:28 +0100

    resolve git conflicts

---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log(4 + 4);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 6));
---
+++ demo.ts
@@ -0,0 +1,8 @@
+console.log(4 + 4);
+
+function add(a:number, b:number){
+return a+ b;
+}
+
+console.log(add(5,6));
+//oozio
